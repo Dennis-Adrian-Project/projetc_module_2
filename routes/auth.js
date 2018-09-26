1be6c1582bde1b09db71ed84e3ff3819d6949475
Dennis <ratenniel@gmail.com>
2018-09-26 14:04:13 +0200

    Ocultación de botónes para Admin 💪🏼

--- routes/auth.js
+++ routes/auth.js
@@ -15,7 +15,7 @@ router.get("/login", (req, res, next) => {
 });
 
 router.post("/login", passport.authenticate("local", {
-  successRedirect: "/auth/profile",
+  successRedirect: "/",
   failureRedirect: "/auth/login",
   failureFlash: true,
   passReqToCallback: true
