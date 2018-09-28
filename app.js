e6a4c6ca2c06644129d4d4c4087223d0cfe8030f
Dennis <ratenniel@gmail.com>
2018-09-28 11:20:46 +0200

    maqueteo

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash = require("connect-flash");
 
 
 mongoose
-  .connect(process.env.DB_URL, { useNewUrlParser: true })
+  .connect(process.env.DB_URL1, { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
