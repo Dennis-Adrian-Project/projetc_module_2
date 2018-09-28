5a7a1b5e5e522da1b31d77ec496d3a5f0dccb58e
Dennis <ratenniel@gmail.com>
2018-09-28 12:02:40 +0200

    maqueteo

--- public/javascripts/event-point.js
+++ public/javascripts/event-point.js
@@ -1,18 +1,23 @@
+const Localizacion = window.log;
 
-
+function startMap(){
 let map = new google.maps.Map(
   document.getElementById('eventMap'),
   {
     zoom: 14,
-    center: center
-  }
-
-  
-const myMarker = new google.maps.Marker({
-  position: {
-    lat: 41.3977381,
-    lng: 2.190471916
-  },
-  map: map,
-  title: ""
-});
+    center: {
+      lat: Localizacion.coordinates[0],
+      lng: Localizacion.coordinates[1]
+    }
+  })
+  const myMarker = new google.maps.Marker({
+    position: {
+      lat: Localizacion.coordinates[0],
+      lng: Localizacion.coordinates[1]
+    },
+    map: map,
+    title: "I'm here"
+  });
+}
+console.log(Localizacion.coordinates)
+startMap();
